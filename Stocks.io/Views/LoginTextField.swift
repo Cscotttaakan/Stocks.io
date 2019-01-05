68642621544ed615a5a69b4019f85d9719571fae
Craig Scott <cscott_taakan@yahoo.com>
2019-01-05 11:14:11 -0600

    Creating Login

---
+++ Stocks.io/Views/LoginTextField.swift
@@ -0,0 +1,21 @@
+//
+//  LoginTextField.swift
+//  Stocks.io
+//
+//  Created by Craig Scott on 1/4/19.
+//  Copyright © 2019 Craig Scott. All rights reserved.
+//
+
+import UIKit
+
+class LoginTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Stocks.io/Views/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  Stocks.io
+//
+//  Created by Craig Scott on 1/5/19.
+//  Copyright © 2019 Craig Scott. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
